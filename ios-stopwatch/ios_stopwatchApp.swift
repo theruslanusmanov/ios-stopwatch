a7a3e59c37fd0606afa9b6bd2eeaff28d1c8b8bd
Ruslan Usmanov <ruslanusm6@gmail.com>
2021-12-26 18:11:11 +0500

    Initial Commit

---
+++ ios-stopwatch/ios_stopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_stopwatchApp.swift
+//  ios-stopwatch
+//
+//  Created by Ruslan Usmanov on 26.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct ios_stopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
